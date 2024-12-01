9aaf3407835d15c0aae80f4585fd4d374da8c964
Guillaume <guillaumeargensse@hotmail.fr>
2024-12-01 11:04:45 +0100

    refactor: now useFetch has a param 'url'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,12 @@ import LogementCard from '../components/LogementCard';
 import { Link } from 'react-router';
 import '../scss/pages/home.scss';
 import { useFetch } from '../utils/customHooks/useFetch';
+//import data from "../../public/data/"
 
 function Home() {
-  const { data, error } = useFetch();
+  const { data, error } = useFetch(
+    'http://localhost:5173/public/data/logements.json',
+  );
 
   if (error) {
     return (
