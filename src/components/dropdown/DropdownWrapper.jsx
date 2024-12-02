fca62c858bbf8a215e7c9909988be1f893b5ddca
Guillaume <guillaumeargensse@hotmail.fr>
2024-12-02 16:54:11 +0100

    feat: add the about page
    about page complete with the related component and style files (dropdown component, dropdown.scss and about.scss

---
+++ src/components/dropdown/DropdownWrapper.jsx
@@ -0,0 +1,56 @@
+/* -------------------------------------------------------------------------- */
+/*                                   IMPORT                                   */
+/* -------------------------------------------------------------------------- */
+
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+import DropdownContent from './DropdownContent';
+
+import '../../scss/components/dropdown.scss';
+
+/* -------------------------------------------------------------------------- */
+/*                                  COMPONENT                                 */
+/* -------------------------------------------------------------------------- */
+
+function DropdownWrapper({ label, content }) {
+  const [isShown, setIsShown] = useState(false);
+
+  const iconClass = isShown ? 'rotate' : '';
+  const labelClass = isShown ? 'show' : '';
+
+  function toggleShow() {
+    setIsShown((prevIsShown) => !prevIsShown);
+  }
+
+  return (
+    <div className={'dropdown'}>
+      <h2 className={`dropdown__label ${labelClass}`} onClick={toggleShow}>
+        {label}
+      </h2>
+      <FontAwesomeIcon
+        icon={faChevronUp}
+        className={`dropdown__icon ${iconClass}`}
+        onClick={toggleShow}
+      />
+      <DropdownContent isShown={isShown} content={content} />
+    </div>
+  );
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                  PROPTYPE                                  */
+/* -------------------------------------------------------------------------- */
+
+DropdownWrapper.propTypes = {
+  label: PropTypes.string.isRequired,
+  content: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.arrayOf(PropTypes.string).isRequired,
+  ]),
+};
+
+export default DropdownWrapper;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,33 @@
+import DropdownWrapper from '../components/dropdown/DropdownWrapper';
+import ImageBanner from '../components/ImageBanner';
+import { useFetch } from '../utils/customHooks/useFetch';
+
+import bannerImg from '../assets/aboutBanner.png';
+import '../scss/pages/about.scss';
+
 function About() {
-  return <div>À propos</div>;
+  const { data, error } = useFetch('http://localhost:5173/data/apropos.json');
+
+  if (error) {
+    return <p>Oups, un problème est survenu</p>;
+  }
+
+  return (
+    <main>
+      <h1 className="sr-only">À propos</h1>
+      <ImageBanner picture={bannerImg} />
+      <section id="dropdownSection">
+        {data &&
+          data.map((elem, index) => (
+            <DropdownWrapper
+              key={`${elem.label}-${index}`}
+              label={elem.label}
+              content={elem.content}
+            />
+          ))}
+      </section>
+    </main>
+  );
 }
 
 export default About;
