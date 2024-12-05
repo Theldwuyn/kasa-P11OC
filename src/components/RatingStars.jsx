013dc5ef27a1910ecf7611f9c37c60e3e3fe5868
Guillaume <guillaumeargensse@hotmail.fr>
2024-12-05 16:46:42 +0100

    feat: add logement page with related component and style

---
+++ src/components/RatingStars.jsx
@@ -0,0 +1,31 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
+import '../scss/components/ratingStars.scss';
+
+function RatingStars({ rating }) {
+  let stars = [];
+  for (let i = 0; i < 5; i++) {
+    i < parseInt(rating) ? stars.push('redStar') : stars.push('');
+  }
+
+  return (
+    <div className="starWrapper">
+      {stars.map((star, index) => (
+        <span key={index}>
+          {star === 'redStar' ? (
+            <FontAwesomeIcon icon={faStar} className="red-star" />
+          ) : (
+            <FontAwesomeIcon icon={faStar} className="grey-star" />
+          )}
+        </span>
+      ))}
+    </div>
+  );
+}
+
+RatingStars.propTypes = {
+  rating: PropTypes.string.isRequired,
+};
+
+export default RatingStars;
--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,5 +1,51 @@
+import { useParams } from 'react-router';
+import { useFetch } from '../utils/customHooks/useFetch';
+import LogementDescription from '../components/LogementDescription';
+import DropdownWrapper from '../components/dropdown/DropdownWrapper';
+
 function Logement() {
-  return <div>Logement</div>;
+  const { logementId: queryId } = useParams();
+  const { data, error } = useFetch('../../data/logements.json');
+
+  function filterData(data, id) {
+    if (!data) return undefined;
+    return data.find((elem) => elem.id === id);
+  }
+
+  const logementData = filterData(data, queryId);
+
+  if (error) {
+    return <p>Oups! Nous ne parvenons pas à récupérer les données</p>;
+  }
+
+  if (logementData) {
+    return (
+      <main>
+        <div>Caroussel</div>
+        <section>
+          <LogementDescription
+            label={logementData.title}
+            location={logementData.location}
+            host={logementData.host}
+            rating={logementData.rating}
+            tags={logementData.tags}
+          />
+          <div className="collapse">
+            <DropdownWrapper
+              label="Description"
+              content={logementData.description}
+            />
+            <DropdownWrapper
+              label="Équipements"
+              content={logementData.equipments}
+            />
+          </div>
+        </section>
+      </main>
+    );
+  } else {
+    return <div>AAAA</div>;
+  }
 }
 
 export default Logement;
