0437cc4a4267cd336943d2af1742f93ff462e967
Guillaume <guillaumeargensse@hotmail.fr>
2024-12-05 10:59:21 +0100

    feat: add error page

--- src/pages/errorPage.jsx
+++ src/pages/errorPage.jsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router';
+import '../scss/pages/errorPage.scss';
+
 function ErrorPage() {
-  return <div>Error</div>;
+  return (
+    <div className="errorPage">
+      <h1 className="errorPage__title">404</h1>
+      <p className="errorPage__message">
+        Oups! La page que vous demandez n&apos;existe pas.
+      </p>
+      <Link to={'/'} className="errorPage__link">
+        Retourner sur la page d&apos;accueil
+      </Link>
+    </div>
+  );
 }
 
 export default ErrorPage;
